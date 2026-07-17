82abeb14a59f7fe530bb9d59c6788afb7c421394
pradeep-tanwar166 <pradeeptanwar166@gmail.com>
2026-07-17 16:09:01 +0530

    6th commit

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -51,6 +51,40 @@ const signup = async(req,res) =>{
     }
 };
 
+const login=async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        // validation 
+
+        if(!email || !password){
+            return res.status(400).json(
+                {
+                    success:false,
+                    message:"Please fill all fields"
+                }
+            )
+        }
+    const user= await User.findOne({email});
+    if(!user){
+        return res.status(404).json(
+            {
+                success:false,
+                message:"User doesn't exist"
+            }
+        )
+    } 
+      const isMatch=await bcrypt.compare(password,user.password);// comparing the password   
+
+      if(!isMatch){
+        return res.status(401).json({
+           success:false,
+           message: "Incorrect email/password", 
+        })
+      }
+    }
+
+}
+
 module.exports = {
     signup
 };
