6b6e71f02f5dd5b1c68dbd0d757aa290f24e6eed
Pranshu Gupta <pranshugupta1423@gmail.com>
2026-07-17 17:15:49 +0530

    8th commit

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -1,30 +1,34 @@
-const User = require("../models/User"); //import user model
+const User = require("../models/User");
+const bcrypt = require("bcryptjs"); 
+const jwt = require("jsonwebtoken"); 
 
-const bcrypt = require("bcryptjs")// import bcrypt
-const jwt=require("jsonwebtoken")
 
+const signup = async (req, res) => {
+    try {
 
-const signup = async(req,res) =>{
-    try{
-        const { fullName, email, phone, password} = req.body;
+     
+        const { fullName, email, phone, password } = req.body;
 
-        if(!fullName || !email || !phone || !password){
+      
+        if (!fullName || !email || !phone || !password) {
             return res.status(400).json({
-                success:false,
-                message: "Please fill your data correctly"
+                success: false,
+                message: "Please fill all fields."
             });
         }
 
-        const existingUser = await User.findOne({email});
+     
+        const existingUser = await User.findOne({ email });
 
-        if(existingUser){
+        if (existingUser) {
             return res.status(400).json({
                 success: false,
-                message: "Email already registered"
-            })
+                message: "Email already registered."
+            });
         }
 
-        const hashedPassword = await bcrypt.hash(password,10);
+
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         const user = new User({
             fullName,
@@ -33,85 +37,118 @@ const signup = async(req,res) =>{
             password: hashedPassword
         });
 
+     
         await user.save();
 
-        res.status(201).json({
-            success:true,
-            message:"Account created successfully."
+        return res.status(201).json({
+            success: true,
+            message: "Account created successfully."
         });
-        
-    }
 
-    catch (error){
+    } catch (error) {
+
         console.log(error);
 
-        res.status(400).json({
-            success:false,
-            message:"Internal Server Error"
-        })
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+
     }
 };
 
-const login=async(req,res)=>{
-    const token=jwt.sign(
-        {
-            id:user._id
-        },
-        process.env.JWT_SECRET,
-        {expiresIn:"7d"}
-    )
-    try{
-        const {email,password}=req.body;
-        // validation 
 
-        if(!email || !password){
-            return res.status(400).json(
-                {
-                    success:false,
-                    message:"Please fill all fields"
-                }
-            )
+const login = async (req, res) => {
+
+    try {
+
+
+        const { email, password } = req.body;
+
+
+        if (!email || !password) {
+
+            return res.status(400).json({
+                success: false,
+                message: "Please fill all fields."
+            });
+
         }
-    const user= await User.findOne({email});
-    if(!user){
-        return res.status(404).json(
-            {
-                success:false,
-                message:"User doesn't exist"
-            }
-        )
-    } 
-      const isMatch=await bcrypt.compare(password,user.password);// comparing the password   
 
-      if(!isMatch){
+
+        const user = await User.findOne({ email });
+
+
+        if (!user) {
+
+            return res.status(404).json({
+                success: false,
+                message: "User doesn't exist."
+            });
+
+        }
+
+
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isMatch) {
+
         return res.status(401).json({
-           success:false,
-           message: "Incorrect email/password", 
-        })
-      }
-     const user= await User.findOne({email});
-      res.status(200).json({
-        sucess:true,
-        message:"Login Successfully",
-        token,
-        user:{
-            id:user._id,
-            fullName:user.fullName,
-            email:user.email,
-            phone:user.phone,
+                success: false,
+                message: "Incorrect email or password."
+            });
 
         }
-      })
+
+        const token = jwt.sign(
+            {
+                id: user._id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+
+        );
+
+        return res.status(200).json({
+
+            success: true,
+            message: "Login Successful.",
+            token,
+            user: {
+                id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                phone: user.phone
+            }
+
+        });
+
+    } catch (error) {
+
+        console.log(error);
+
+        return res.status(500).json({
+
+            success: false,
+
+            message: "Internal Server Error"
+
+        });
+
     }
-catch(error){
-    console.log(error);
-    res.status(500).json({
-        sucess:false,
-        message:"Internal server error"
-    })
-}
-}
+
+};
+
 
 module.exports = {
-    signup,login
+
+    signup,
+
+    login
+
 };
