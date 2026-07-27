3b9866d1a79a9afab792cdc7d0179fd4d7ca1172
Mehak <mehakaroraaa111@gmail.com>
2026-07-27 16:51:38 +0530

    24th commit

---
+++ Frontend/src/Theme/shadows.js
@@ -0,0 +1,40 @@
+export const shadow={
+    soft:{
+        shadowColor:"#000000",
+        shadowOpacity:0.03,
+        shadowRadius:6,
+        shadowOffset:{width:0,height:2},
+        elevation:2,
+        
+
+    },
+     medium:{
+        shadowColor:"#000000",
+        shadowOpacity:0.06,
+        shadowRadius:10,
+        shadowOffset:{width:0,height:4},
+        elevation:4,
+        
+
+    },
+     primaryGlow:{
+        shadowColor:"#7C3AED",
+        shadowOpacity:0.25,
+        shadowRadius:12,
+        shadowOffset:{width:0,height:6},
+        elevation:6,
+        
+
+    },
+     lavenderGlow:{
+        shadowColor:"#A78BFA",
+        shadowOpacity:0.15,
+        shadowRadius:10,
+        shadowOffset:{width:0,height:4},
+        elevation:4,
+        
+
+    }
+}
+
+
---
+++ Frontend/src/Theme/typography.js
@@ -0,0 +1,18 @@
+export const typography={
+    sizes:{
+        h1:28,
+        h2:22,
+        h3:18,
+        large:16,
+        body:14,
+        small:12,
+        caption:10,
+    },
+    weights:{
+        bold:"800",
+        semiBold:"600",
+        medium:"500",
+        regular:"400",
+    }
+
+}
