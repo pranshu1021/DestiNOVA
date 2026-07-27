2322f5311015c31900409020a4f4994b71484778
pradeep-tanwar166 <pradeeptanwar166@gmail.com>
2026-07-27 17:40:21 +0530

    27th commit

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-const { signup,googleLogin, login, getProfile} = require("../controllers/authControllers");
+const { signup,googleLogin, login, getProfile,updateProfile} = require("../controllers/authControllers");
 
 const protect = require("../middleware/authMiddleware");
 router.get("/profile",protect,getProfile);
 router.post("/signup",signup);
 router.post("/login",login);
 router.post("/google", googleLogin);
+router.put("/update-profile",protect,updateProfile);
 module.exports = router;
